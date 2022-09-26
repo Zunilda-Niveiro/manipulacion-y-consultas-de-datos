1397563e4200c2543b94d705238a6d8e5feec748
Zunilda-Niveiro <niveiro.zunilda@gmail.com>
2022-09-26 16:25:00 -0300

    subo msql del tp manipulacion de datos

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT first_name, last_name, rating FROM actors
+WHERE rating > 7.5;
+
+SELECT title, rating, awards FROM movies
+WHERE rating > 7.5
+AND awards > 2;
+
+SELECT title, rating FROM movies
+ORDER BY rating ASC; 
---
+++ tp_movies.sql
@@ -0,0 +1,11 @@
+INSERT INTO `movies_db`.`genres` 
+(`name`, `ranking`, `active`) VALUES ('investigacion', '13', '1');
+
+UPDATE `movies_db`.`genres` SET `name`=' investigacion cientifica' WHERE `id`='13';
+
+DELETE FROM `movies_db`.`genres` WHERE `id`='13';
+
+
+SELECT * FROM movies;
+
+SELECT first_name, last_name, rating FROM actors;
